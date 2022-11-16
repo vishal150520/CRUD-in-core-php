7f1ee85b231e99fcc9d73ab393f1e1ecf8f73afa
vishal150520 <vishalkumar150520@gmail.com>
2022-11-16 18:12:54 +0530

    .

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php 
+
+include "config.php"; 
+
+if (isset($_GET['id'])) {
+
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM `vishaldata` WHERE `id`='$user_id'";
+
+     $result = $conn->query($sql);
+
+     if ($result == TRUE) {
+        echo "Record deleted successfully.";
+        
+        header("location:view.php");
+    }else{
+
+        echo "Error:" . $sql . "<br>" . $conn->error;
+
+    }
+
+} 
+
+?>
